c130cb8a85be97ef131b41f8726903fa2cdb2d8a
Yash Gupta <ygupta@ucsc.edu>
2019-01-26 19:08:45 -0800

    Moved to kernel implementation of synced i/o

--- src/dm_afs_io.c
+++ src/dm_afs_io.c
@@ -7,19 +7,6 @@
 #include <dm_afs_modules.h>
 
 /**
- * Callback function to signify the completion 
- * of an IO transfer to a block device.
- */
-static void
-__callback_blkdev_io(struct bio *bio)
-{
-    struct completion *event = bio->bi_private;
-
-    //afs_debug("completed event: %p\n", event);
-    complete(event);
-}
-
-/**
  * Read or write to an block device.
  * 
  * @request  I/O request structure
@@ -32,15 +19,6 @@ afs_blkdev_io(struct afs_io *request)
     const int page_offset = 0;
     int ret;
     struct bio *bio = NULL;
-    struct completion event;
-
-    // Any IO to a block device in the kernel is done using bio.
-    // We need to allocate a bio with a single IO vector and set
-    // its corresponding fields.
-    //
-    // bio's are asynchronous and hence an event based waiting
-    // loop is used to provide the illusion of synchronous IO.
-    init_completion(&event);
 
     bio = bio_alloc(GFP_NOIO, 1);
     afs_assert_action(!IS_ERR(bio), ret = PTR_ERR(bio), alloc_err, "could not allocate bio [%d]", ret);
@@ -57,18 +35,12 @@ afs_blkdev_io(struct afs_io *request)
     default:
         afs_assert_action(0, ret = -EINVAL, invalid_type, "invalid IO type [%d]", request->type);
     }
-    // bio->bi_opf |= REQ_SYNC;
-    // TODO: Perhaps make ASYNC for performance considerations?
 
     bio_set_dev(bio, request->bdev);
     bio->bi_iter.bi_sector = request->io_sector;
-    bio->bi_private = &event;
-    bio->bi_end_io = __callback_blkdev_io;
     bio_add_page(bio, request->io_page, request->io_size, page_offset);
 
-    submit_bio(bio);
-    wait_for_completion(&event);
-
+    submit_bio_wait(bio);
     return 0;
 
 invalid_type:
