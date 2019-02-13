d408c95727f2c816de61c19d2eb4a7ae35dcb200
austen barker <atbarker@ucsc.edu>
2019-02-12 20:42:53 -0800

    header file prototypes, all that should be needed

--- entropy-test/dm_afs_entropy.h
+++ entropy-test/dm_afs_entropy.h
@@ -1,5 +1,12 @@
 #ifndef ENTROPY_H
 #define ENTROPY_H
 
+#include <linux/slab.h>
+
+void build_entropy_ht(char* directory_name);
+
+void allocate_entropy(uint8_t* filename_hash, uint32_t block_pointer);
+
+int read_entropy(uint8_t* filename_hash, uint32_t block_pointer, uint8_t* block);
 
 #endif
