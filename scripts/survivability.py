26bf38850ec9b2102a815d64a0eb996249ad0111
Austen Barker <atbarker@ucsc.edu>
2019-06-06 15:14:46 -0700

    refined script

--- scripts/survivability.py
+++ scripts/survivability.py
@@ -19,46 +19,49 @@ def prob_survival(k, m, p):
     return binom.cdf(m, k+m, p)
 
 def prob_survival_artifice(art_size, k, m, p):
-    return (prob_survival(k, m, p)) ** art_size
+    return (prob_survival(k, m, p)) ** art_size_blocks
 
-def calc_metadata_size_rs(blocks, parity, entropy, data):
-    #all measurements are in bytes
+def calc_metadata_size_rs(blocks, parity, entropy, data, replicas):
     pointer_size = 4
     small_checksum = 2
     art_block_hash = 16
     entropy_filename_hash = 8
-    superblock_replicas = 8
+    print("Codeword configuration: {} data blocks, {} entropy, {} parity blocks".format(data, entropy, parity))
 
     amplification_factor = parity / data
-    print("amplification factor: {}".format(amplification_factor))
+    print("write amplification factor: {}".format(amplification_factor))
     carrier_block_tuple = pointer_size + small_checksum
     #I forget what the 4 is supposed to do, probably to align everything to an easy block boundary
-    record_size = ((entropy + parity) * carrier_block_tuple) + art_block_hash + entropy_filename_hash + 4
+    record_size = (parity * carrier_block_tuple) + (entropy * pointer_size) + art_block_hash + entropy_filename_hash
     print("record size: {}".format(record_size))
     pointers_per_pointerblock = (block_size / pointer_size - 1)
 
     entries_per_block = math.floor(block_size / record_size)
     print("entries per block: {}".format(entries_per_block))
-    num_map_blocks = math.ceil((blocks / amplification_factor) / entries_per_block)
+    num_map_blocks = math.ceil((blocks / data) / entries_per_block)
     print("number of map blocks: {}".format(num_map_blocks))
-    num_map_map_blocks = math.ceil((num_map_blocks / amplification_factor) / entries_per_block)
+    num_map_map_blocks = math.ceil((num_map_blocks / data) / entries_per_block)
     print("number of map map blocks: {}".format(num_map_map_blocks))
     num_pointer_blocks = math.ceil(num_map_map_blocks / pointers_per_pointerblock)
     print("number of pointer_blocks: {}".format(num_pointer_blocks))
-    total_size = ((num_pointer_blocks + 1) * superblock_replicas) + num_map_map_blocks + num_map_blocks
-    print("total size in blocks: {}".format(total_size))
-    return total_size
+    metadata_size = ((num_pointer_blocks + 1) * replicas) + num_map_map_blocks + num_map_blocks
+    print("total size in blocks: {}".format(metadata_size))
+    metadata_overhead = (metadata_size / blocks) * 100
+    print("metadata overhead: {} percent".format(metadata_overhead))
+    effective_artifice_size = (art_size_blocks * amplification_factor) + metadata_size
+    print("effective artifice size: {}".format(effective_artifice_size))
+    return metadata_size
 
 
-def calc_metadata_size_shamir(blocks, parity, data):
-    #all measurements are in bytes
+def calc_metadata_size_shamir(blocks, parity, data, replicas):
     pointer_size = 4
     small_checksum = 2
     art_block_hash = 16
-    superblock_replicas = 8
+
+    print("Codeword configuration: {} data blocks, {} parity blocks".format(data, parity))
 
     amplification_factor = parity / data
-    print("amplification factor: {}".format(amplification_factor))
+    print("write amplification factor: {}".format(amplification_factor))
     carrier_block_tuple = pointer_size + small_checksum
     record_size = (parity * carrier_block_tuple) + art_block_hash
     print("record size: {}".format(record_size))
@@ -72,29 +75,33 @@ def calc_metadata_size_shamir(blocks, parity, data):
     print("number of map map blocks: {}".format(num_map_map_blocks))
     num_pointer_blocks = math.ceil(num_map_map_blocks / pointers_per_pointerblock)
     print("number of pointer_blocks: {}".format(num_pointer_blocks))
-    total_size = ((num_pointer_blocks + 1) * superblock_replicas) + num_map_map_blocks + num_map_blocks
-    print("total size in blocks: {}".format(total_size))
-    return total_size
+    metadata_size = ((num_pointer_blocks + 1) * replicas) + num_map_map_blocks + num_map_blocks
+    print("total size in blocks: {}".format(metadata_size))
+    metadata_overhead = (metadata_size / blocks) * 100
+    print("metadata overhead: {} percent".format(metadata_overhead))
+    effective_artifice_size = (art_size_blocks * amplification_factor) + metadata_size
+    print("effective artifice size: {}".format(effective_artifice_size))
+    return metadata_size
 
 def calc_total_size(size, parity, data):
     return size * (parity / data)
 
 def prob_metadata_alive(k, m):
-    return (prob_survival(k, m, prob_success) ** (calc_metadata_size_rs(art_size_blocks, m, 2, 2) * num_days))
+    return (prob_survival(k, m, prob_success) ** (calc_metadata_size_rs(art_size_blocks, m, 2, 2, 8) * num_days))
 
 def prob_artifice_alive(k, m):
-    return (prob_survival(k, m, prob_success) ** (calc_total_size(art_size_blocks, m, k) * num_days))
+    return (prob_survival(k, m, prob_success) ** (calc_total_size(art_size_blocks, m, k, 8) * num_days))
 
 #mean time to failure (MTTF) is in hours
 def prob_disk_alive(mttf, days):
     return ((1 - (1/mttf)) ** (days * 24))
 
 def main():
-    print("Metadata_size for Reed-Solomon")
-    calc_metadata_size_rs(art_size_blocks, 4, 2, 2)
+    print("|---Metadata size for Reed-Solomon-----|")
+    calc_metadata_size_rs(art_size_blocks, 4, 1, 2, 8)
     print(" ")
-    print("Metadata size for shamir secret sharing")
-    calc_metadata_size_shamir(art_size_blocks, 4, 1)
+    print("|---Metadata size for secret sharing---|")
+    calc_metadata_size_shamir(art_size_blocks, 4, 1, 8)
 
 if __name__ == "__main__":
     main()
