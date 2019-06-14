afc4c3fa3ccf4b640c2b056db746b82668bd29d1
Austen Barker <atbarker@ucsc.edu>
2019-06-14 11:08:42 -0700

    meaningful results

--- scripts/survivability.py
+++ scripts/survivability.py
@@ -16,11 +16,11 @@ num_days = 365
 
 #in matlab this is represented as Sum[PDF[BinomialDistribution[k+m,p],i], {i, 0, m}]
 #as this is just a cumulative distribution function, python can do this for us
-def prob_survival(k, m, p):
+def prob_survival_sss(k, m, p):
     return binom.cdf(m, k+m, p)
 
-def prob_survival_artifice(art_size, k, m, p):
-    return (prob_survival(k, m, p)) ** art_size_blocks
+def prob_survival_rs(e, d, m, p):
+    return binom.cdf(m-d, e+m, p)
 
 def calc_metadata_size_rs(blocks, parity, entropy, data, replicas, verbose):
     pointer_size = 4
@@ -42,6 +42,7 @@ def calc_metadata_size_rs(blocks, parity, entropy, data, replicas, verbose):
     effective_artifice_size = (art_size_blocks * amplification_factor) + metadata_size
 
     if verbose == True:
+        print("|---Metadata size for Reed-Solomon-----|")
         print("Codeword configuration: {} data blocks, {} entropy, {} parity blocks".format(data, entropy, parity))
         print("Write amplification factor: {}".format(amplification_factor))
         print("Record Size: {} bytes".format(record_size))
@@ -52,18 +53,20 @@ def calc_metadata_size_rs(blocks, parity, entropy, data, replicas, verbose):
         print("Metadata size in blocks: {}".format(metadata_size))
         print("Metadata overhead: {} percent".format(metadata_overhead))
         print("effective artifice size: {}".format(effective_artifice_size))
+        print("")
 
     return metadata_size
 
-
-def calc_metadata_size_shamir(blocks, parity, data, replicas, verbose):
+#Assumes that there is only 1 data block per code word.
+#
+def calc_metadata_size_shamir(blocks, shares, threshold, replicas, verbose):
     pointer_size = 4
     small_checksum = 2
     art_block_hash = 16
 
-    amplification_factor = parity / data
+    amplification_factor = shares
     carrier_block_tuple = pointer_size + small_checksum
-    record_size = (parity * carrier_block_tuple) + art_block_hash
+    record_size = (shares * carrier_block_tuple) + art_block_hash
     pointers_per_pointerblock = (block_size / pointer_size) - 1
 
     entries_per_block = math.floor(block_size / record_size)
@@ -75,8 +78,9 @@ def calc_metadata_size_shamir(blocks, parity, data, replicas, verbose):
     effective_artifice_size = (art_size_blocks * amplification_factor) + metadata_size
    
     if verbose == True:
-        print("Codeword configuration: {} data blocks, {} parity blocks".format(data, parity))
-        print("Write amplification factor: {}".format(amplification_factor))
+        print("|---Metadata size for Secret Sharing-----|")
+        print("Codeword configuration: reconstruct threshold {}, {} shares".format(threshold, shares))
+        print("Write amplification factor: {}".format(shares))
         print("Record Size: {} bytes".format(record_size))
         print("Entries per map block: {}".format(entries_per_block))
         print("Number of map blocks: {}".format(num_map_blocks))
@@ -85,6 +89,7 @@ def calc_metadata_size_shamir(blocks, parity, data, replicas, verbose):
         print("Metadata size in blocks: {}".format(metadata_size))
         print("Metadata overhead: {} percent".format(metadata_overhead))
         print("effective artifice size: {}".format(effective_artifice_size))
+        print("")
 
     return metadata_size
 
@@ -93,32 +98,32 @@ def calc_total_size(size, parity, data):
 
 #TODO both of these need some work so that we can more accurately model overwrite resistance with redundant metadata
 #hard coded for 1 entropy block, multiple data blocks
-def prob_metadata_alive_rs(k, m):
-    return math.pow(prob_survival(k, m, prob_success), (calc_metadata_size_rs(art_size_blocks, m, 1, k-1, 8, False) * num_days))
+def prob_metadata_alive_rs(e, d, m):
+    return math.pow(prob_survival_rs(e, d, m, prob_success), (calc_metadata_size_rs(art_size_blocks, m, e, d, 8, False) * num_days))
 
+#in this case k is the reconstruct threshold, m is the number of additional shares
 def prob_metadata_alive_sss(k, m):
-    return math.pow(prob_survival(k, m, prob_success), (calc_metadata_size_shamir(art_size_blocks, m, k, 8, False) * num_days))
+    return math.pow(prob_survival_sss(k, m, prob_success), (calc_metadata_size_shamir(art_size_blocks, m, k, 8, False) * num_days))
 
-def prob_artifice_alive_rs(k, m):
-    return math.pow(prob_survival(k, m, prob_success), (calc_total_size(art_size_blocks, m, k, 8, False) * num_days))
+#k is data + entropy, m 
+def prob_artifice_alive_rs(e, d, m):
+    return math.pow(prob_survival_rs(e, d, m, prob_success), (calc_total_size(art_size_blocks, m, e+d, 8, False) * num_days))
 
+#k is the reconstruct threshold, m is the number of additional shares
 def prob_artifice_alive_sss(k, m):
-    return math.pow(prob_survival(k, m, prob_success), (calc_total_size(art_size_blocks, m, k, 8, False) * num_days))
+    return math.pow(prob_survival_sss(k, m, prob_success), (calc_total_size(art_size_blocks, m, k, 8, False) * num_days))
 
 def prob_nines(k, m, p):
     return -math.log10(1 - prob_survival(k, m, p))
 
 #mean time to failure (MTTF) is in hours
+#calculates the probability that a given disk will die
 def prob_disk_alive(mttf, days):
     return ((1 - (1/mttf)) ** (days * 24))
 
 def main():
-    smooth = True
-    print("|---Metadata size for Reed-Solomon-----|")
-    calc_metadata_size_rs(art_size_blocks, 4, 1, 2, 8, True)
-    print(" ")
-    print("|---Metadata size for secret sharing---|")
-    calc_metadata_size_shamir(art_size_blocks, 4, 1, 8, True)
+    #calc_metadata_size_rs(art_size_blocks, 4, 1, 2, 8, True)
+    #calc_metadata_size_shamir(art_size_blocks, 4, 1, 8, True)
     
     #nines
     '''m_max = 0.05
@@ -143,17 +148,24 @@ def main():
     prob1 = []
     prob2 = []
     prob3 = []
+    prob4 = []
     for i in m_values:
-        prob1.append(prob_metadata_alive_rs(2, i))
-        prob2.append(prob_metadata_alive_sss(1, i))
-        prob3.append(prob_metadata_alive_rs(3, i))
+        #1 entropy block, 1 data block, i parity blocks
+        prob1.append(prob_metadata_alive_rs(1, 1, i))
+        #reconstruct threshold of 2, i additional blocks
+        prob2.append(prob_metadata_alive_sss(2, i))
+        #1 entropy block, 2 data blocks, i parity blocks
+        prob3.append(prob_metadata_alive_rs(1, 2, i))
+        #reconstruct threshold of 3, i additional blocks
+        prob4.append(prob_metadata_alive_sss(3, i))
 
     plt.xlabel("Number of Parity Blocks")
     plt.ylabel("Probability of Survival")
-    plt.title("Probability of Survival vs Number of Parity Blocks")
+    plt.title("Probability of Metadata Survival vs Number of Parity Blocks")
     rs = plt.plot(m_values, prob1, label='RS, 1 data block')
     rs2 = plt.plot(m_values, prob3, label='RS, 2 data blocks')
-    shamir = plt.plot(m_values, prob2, label='secret sharing')
+    shamir = plt.plot(m_values, prob2, label='SSS, threshold 2')
+    shamir2 = plt.plot(m_values, prob4, label='SSS, threshold 3')
     plt.legend()
     plt.show()
 
