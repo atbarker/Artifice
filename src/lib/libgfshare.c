bce1b644492cb515299e43682e729449213bde8e
Austen Barker <atbarker@ucsc.edu>
2019-07-16 13:03:46 -0700

    speck RNG

--- src/lib/libgfshare.c
+++ src/lib/libgfshare.c
@@ -144,7 +144,81 @@ static void _gfshare_fill_rand_using_random_bytes(uint8_t* buffer, size_t count)
     get_random_bytes(buffer, count);
 }
 
-gfshare_rand_func_t gfshare_fill_rand = _gfshare_fill_rand_using_random_bytes;
+#define BLOCK_SIZE 16
+
+#define ROR(x, r) ((x >> r) | (x << (64 - r)))
+#define ROL(x, r) ((x << r) | (x >> (64 - r)))
+#define R(x, y, k) (x = ROR(x, 8), x += y, x ^= k, y = ROL(y, 3), y ^= x)
+#define ROUNDS 32
+
+/**
+ * pt: plaintext
+ * ct: ciphertext
+ * k: key
+ * we assume that input arrays are of length 2 so we get 128 bits
+ * Should generate the key on the fly, just for simplicity sake
+ * Better performance can be had by computing round keys once.
+ * This function is obtained from the following paper, https://eprint.iacr.org/2013/404
+ */
+void speck_encrypt(uint64_t ct[2], uint64_t const pt[2], uint64_t const K[2])
+{
+    uint64_t y = pt[0], x = pt[1], b = K[0], a = K[1];
+    int i = 0;
+    R(x, y, b);
+    for (i = 0; i < ROUNDS - 1; i++) {
+        R(a, b, i);
+        R(x, y, b);
+    }
+
+    ct[0] = y;
+    ct[1] = x;
+}
+
+/**
+ * output_length: size of the output block
+ * output_block: destination for pseudorandom bits
+ * seed: a 128 bit random number
+ * Generate a block of random bytes given a key (seed) by running speck in counter mode
+ * block input should be aligned to 128 bit (16 byte)  boundaries
+ * We assume a length of 16 bytes (128 bits) for the seed.
+ */
+void generate_block_ctr(size_t output_length, uint8_t *output_block, uint8_t *seed){
+    uint32_t rounds = output_length/BLOCK_SIZE;
+    uint64_t i, ctr[2], key[2], output[2];
+    uint64_t j = 0;
+
+    if(output_length % BLOCK_SIZE != 0){
+        printk(KERN_INFO "Not aligned to 128 bit boundary %ld", output_length);
+    }
+
+    key[0] = ((uint64_t *)seed)[0];
+    key[1] = ((uint64_t *)seed)[1];
+
+    ctr[0] = 0;
+    ctr[1] = 0;
+
+    for(i = 0; i < rounds; i++){
+       speck_encrypt(output, ctr, key);
+       ((uint64_t *)output_block)[j + 1] = output[1];
+       ((uint64_t *)output_block)[j + 0] = output[0];
+       ctr[0]++;
+       j += 2;
+    }
+}
+
+uint64_t * get_seed_64(void){
+    static uint64_t random[2];
+    get_random_bytes(random, 16);
+    return random;
+}
+
+static void _gfshare_fill_rand_using_speck(uint8_t* buffer, size_t count){
+    uint8_t *key = (uint8_t*)get_seed_64();
+    generate_block_ctr(count, buffer, key);
+}
+
+
+gfshare_rand_func_t gfshare_fill_rand = _gfshare_fill_rand_using_speck;
 
 
 /* ------------------------------------------------------[ Preparation ]---- */
@@ -229,10 +303,10 @@ int gfshare_ctx_setsize(gfshare_ctx* ctx, size_t size) {
 /* Free a share context's memory. */
 void gfshare_ctx_free(gfshare_ctx* ctx) {
   gfshare_fill_rand( ctx->buffer, ctx->sharecount * ctx->maxsize );
-  gfshare_fill_rand( ctx->sharenrs, ctx->sharecount );
+  _gfshare_fill_rand_using_random_bytes( ctx->sharenrs, ctx->sharecount );
   kfree( ctx->sharenrs );
   kfree( ctx->buffer );
-  gfshare_fill_rand( (uint8_t*)ctx, sizeof(struct _gfshare_ctx) );
+  _gfshare_fill_rand_using_random_bytes( (uint8_t*)ctx, sizeof(struct _gfshare_ctx) );
   kfree( ctx );
 }
 
