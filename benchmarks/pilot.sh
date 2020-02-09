a97cab29e47875afd213964994d5c6fb19c01402
Austen Barker <atbarker@ucsc.edu>
2020-02-09 09:22:18 -0800

    modified script

--- benchmarks/pilot.sh
+++ benchmarks/pilot.sh
@@ -2,4 +2,5 @@ testdir=$1
 user=$3
 ram=$2
 
-bench run_program --pi "throughput,KB/s,0,1,1" -- ./bonnie-pilot.sh $testdir $ram $user
+#9 for write throughput, 15 for read throughput
+bench run_program --pi "throughput,KB/s,9,1,1" -- ./bonnie-pilot.sh $testdir $ram $user
